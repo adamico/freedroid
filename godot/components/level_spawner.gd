9402f8af66d7bf99cc10f4d715195c3e85004bd0
Andrea D'Amico <andrea.damico@gmail.com>
2026-03-08 10:49:16 +0100

    fix: remove droid spaw over time

--- godot/components/level_spawner.gd
+++ godot/components/level_spawner.gd
@@ -2,13 +2,11 @@ class_name LevelSpawner
 extends Node
 
 @export var level_number: int = 0
-@export var spawn_interval: float = 5.0
-var _spawn_data: DroidSpawnData
-var _level_data: LevelData
-var _spawn_timer: float = 0.0
 
 var _enemies_spawned: int = 0
 var _level_completed: bool = false
+var _level_data: LevelData
+var _spawn_data: DroidSpawnData
 
 var enemy_scene: PackedScene = preload("res://entities/enemy/enemy.tscn")
 
@@ -31,21 +29,15 @@ func _ready() -> void:
 	if _spawn_data:
 		# Initial spawn of special forces
 		_spawn_special_forces()
+		_spawn_random_droids()
 
-	_spawn_timer = spawn_interval
 
-
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	if not _spawn_data or not _level_data:
 		return
 
 	if not _level_completed:
 		_check_level_completed()
-
-	_spawn_timer -= delta
-	if _spawn_timer <= 0.0:
-		_spawn_timer = spawn_interval
-		_try_spawn_random_droid()
 
 
 func _spawn_special_forces() -> void:
@@ -68,54 +60,34 @@ func _spawn_special_forces() -> void:
 		_enemies_spawned += 1
 
 
-func _try_spawn_random_droid() -> void:
-	if _spawn_data.allowed_droid_types.is_empty():
+func _spawn_random_droids() -> void:
+	if _spawn_data.allowed_droid_types.is_empty() or _level_data.waypoints.is_empty():
 		return
 
-	var current_droids = get_tree().get_nodes_in_group("enemy")
-	var count = 0
-	# Only count enemies in this level
-	for d in current_droids:
-		if d.get_parent() == get_parent():
-			count += 1
-
-	if count >= _spawn_data.max_random_droids:
-		return
-
-	# Prevent spawning more than the maximum total allowed
-	var expected_special_forces_count = _spawn_data.special_forces.size()
-	var max_total_droids = expected_special_forces_count + _spawn_data.max_random_droids
-	if _enemies_spawned >= max_total_droids:
-		return
-
-	# Pick random type
 	var types = _spawn_data.allowed_droid_types
-	var r_type = types[randi() % types.size()]
-
-	var type_res = _load_droid_data(r_type)
-	if not type_res:
-		return
-
-	# Pick random waypoint
-	if _level_data.waypoints.is_empty():
-		return
-	var wp = _level_data.waypoints[randi() % _level_data.waypoints.size()]
 	var half := GameConstantsData.TILE_SIZE / 2.0
-	var pos = Vector2(wp.position) * GameConstantsData.TILE_SIZE \
-	+ Vector2(half, half)
 
-	var enemy = enemy_scene.instantiate() as Node2D
-	# Let's set the resource based on the structure of enemy.tscn
-	if enemy.has_method("set_droid_data"):
-		enemy.set_droid_data(type_res)
-	elif "droid_data" in enemy:
-		enemy.set("droid_data", type_res)
+	for i in range(_spawn_data.max_random_droids):
+		var r_type = types[randi() % types.size()]
+		var type_res = _load_droid_data(r_type)
+		if not type_res:
+			continue
 
-	enemy.global_position = pos
-	enemy.add_to_group("enemy")
-	_inject_ai_data(enemy, type_res)
-	get_parent().add_child(enemy)
-	_enemies_spawned += 1
+		var wp = _level_data.waypoints[randi() % _level_data.waypoints.size()]
+		var pos = Vector2(wp.position) * GameConstantsData.TILE_SIZE \
+		+ Vector2(half, half)
+
+		var enemy = enemy_scene.instantiate() as Node2D
+		if enemy.has_method("set_droid_data"):
+			enemy.set_droid_data(type_res)
+		elif "droid_data" in enemy:
+			enemy.set("droid_data", type_res)
+
+		enemy.global_position = pos
+		enemy.add_to_group("enemy")
+		_inject_ai_data(enemy, type_res)
+		get_parent().add_child(enemy)
+		_enemies_spawned += 1
 
 
 func _load_droid_data(droid_name: String) -> DroidData:
